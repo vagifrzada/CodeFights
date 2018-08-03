55b6ebba6ec6f9ee6c066b125eeb558f891fd054
Vagif Rufullazada <vaqifrzade@gmail.com>
2018-08-03 16:41:21 +0100

    Added newNumeralSystem

---
+++ newNumeralSystem/code.php
@@ -0,0 +1,25 @@
+<?php
+
+function newNumeralSystem(string $number): array
+{
+    $out = [];
+
+    $letters = [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+        'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+        'X', 'Y', 'Z'
+    ];
+
+    $decimal = array_search($number, $letters);
+    $count = count($letters);
+
+    for ($i = 0; $i < $count; $i++) {
+        for ($x = $count; $x >= $i; $x--) {
+            if ($i + $x == $decimal) {
+                $out[] = "{$letters[$i]} + {$letters[$x]}";
+            }
+        }
+    }
+
+    return $out;
+}
