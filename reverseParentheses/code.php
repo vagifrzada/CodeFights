7d1002a9fd152335727601dba4c49e56a604473e
Vagif Rufullazada <vaqifrzade@gmail.com>
2018-08-24 16:49:59 +0100

    Code refactored

--- reverseParentheses/code.php
+++ reverseParentheses/code.php
@@ -6,24 +6,9 @@
  */
 function reverseParentheses(string $s): string
 {
-    if (strpos($s, '(') !== false) {
-        return reverseParentheses(
-            reverse($s)
-        );
+    while (preg_match('/\(([^()]*)\)/im', $s, $matches)) {
+       $s = str_replace($matches[0], strrev($matches[1]), $s);
     }
 
     return $s;
 }
-
-/**
- * @param string $s
- * @return string
- */
-function reverse(string $s): string
-{
-    $regex = '/\(([^()]*)\)/im';
-    preg_match($regex, $s, $matches);
-    // return preg_replace($regex, strrev($matches[1]), $s);
-    return str_replace('('. $matches[1] .')', strrev($matches[1]), $s);
-}
-
